62bbecd7898adf3cdfec278873ea0ecc489526c2
hoshikira18 <khuyen.dev183@gmail.com>
2025-01-10 12:15:06 +0700

    feat: config route, mantine ui

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <div>Hello, ViteJS</div>;
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LoginPage />,
+    },
+    {
+        path: "/main/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/main/",
+                element: <div>Home</div>,
+            },
+            {
+                path: "/main/about",
+                element: <div>About</div>,
+            },
+        ],
+    },
+]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <MantineProvider theme={theme}>
+            <Notifications />
+            <RouterProvider router={router} />
+        </MantineProvider>
     </StrictMode>,
 );

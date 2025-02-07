1eb12f6d545994074d91a0de0f5febf64e858172
SpringThanh <nastuthanh2@gmail.com>
2025-02-07 21:54:30 +0700

    refactor: shipper manager page

--- src/pages/ShipperManagementPage.jsx
+++ src/pages/ShipperManagementPage.jsx
@@ -6,74 +6,64 @@ const ShipperManagementPage = () => {
         {
             id: 1,
             name: "Nguyễn Văn An",
-            email: "nguyenvanan@gmail.com",
-            role: "Shipper",
-            status: "Pending",
+            phone: "0923457789",
+            activityInformation: "Giao hàng chung cư Vincom Park Place",
+            shippingMethod: "Xe máy",
+            status: "Đang hoạt động",
         },
         {
             id: 2,
             name: "Trần Thị Bích",
-            email: "tranthibich@gmail.com",
-            role: "Shipper",
-            status: "Pending",
+            phone: "0987654321",
+            activityInformation: "Giao hàng chung cư Keangnam Hanoi Landmark Tower",
+            shippingMethod: "Xe đạp",
+            status: "Đang hoạt động",
         },
         {
             id: 3,
             name: "Lê Văn Cần",
-            email: "levancan@gmail.com",
-            role: "Shipper",
-            status: "Pending",
+            phone: "0912345678",
+            activityInformation: "Giao hàng chung cư Royal City",
+            shippingMethod: "Xe máy",
+            status: "Đang hoạt động",
         },
         {
             id: 4,
             name: "Phạm Thị Dung",
-            email: "phamthidung@gmail.com",
-            role: "Shipper",
-            status: "Pending",
+            phone: "0901234567",
+            activityInformation: "Giao hàng chung cư The Link Ciputra",
+            shippingMethod: "Xe máy",
+            status: "Đang hoạt động",
         },
     ]);
 
     const navigate = useNavigate();
 
-    // Filter out only the shippers with "Pending" status
-    const pendingOperators = operators.filter((operator) => operator.status === "Pending");
-
-    // Navigate to the shipper detail page
-    const handleViewDetails = (operator) => {
-        navigate(`/main/operators/${operator.id}`, { state: { operator } });
-    };
-
     return (
         <div className="container mx-auto p-6">
-            <h1 className="text-2xl font-bold mb-4">List of Shippers in Pending Status</h1>
+            <h1 className="text-2xl font-bold mb-4">Danh sách shipper</h1>
             <table className="min-w-full table-auto border-collapse border border-gray-200">
                 <thead>
                     <tr>
                         <th className="border px-4 py-2">ID</th>
-                        <th className="border px-4 py-2">Name</th>
-                        <th className="border px-4 py-2">Email</th>
-                        <th className="border px-4 py-2">Role</th>
-                        <th className="border px-4 py-2">Status</th>
-                        <th className="border px-4 py-2">Actions</th>
+                        <th className="border px-4 py-2">Họ và Tên</th>
+                        <th className="border px-4 py-2">Số điện thoại</th>
+                        <th className="border px-4 py-2">Phạm vi hoạt động</th>
+                        <th className="border px-4 py-2">Phương thức vận chuyển</th>
+                        <th className="border px-4 py-2">Trạng thái hoạt động</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {pendingOperators.map((operator) => (
+                    {operators.map((operator) => (
                         <tr key={operator.id}>
                             <td className="border px-4 py-2 text-center">{operator.id}</td>
                             <td className="border px-4 py-2">{operator.name}</td>
-                            <td className="border px-4 py-2">{operator.email}</td>
-                            <td className="border px-4 py-2">{operator.role}</td>
-                            <td className="border px-4 py-2">{operator.status}</td>
-                            <td className="border px-4 py-2 flex justify-center space-x-4">
-                                <button
-                                    type="button"
-                                    onClick={() => handleViewDetails(operator)}
-                                    className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300"
-                                >
-                                    View
-                                </button>
+                            <td className="border px-4 py-2 text-center">{operator.phone}</td>
+                            <td className="border px-4 py-2">{operator.activityInformation}</td>
+                            <td className="border px-4 py-2 text-center">
+                                {operator.shippingMethod}
                             </td>
+                            <td className="border px-4 py-2 text-center">{operator.status}</td>
                         </tr>
                     ))}
                 </tbody>

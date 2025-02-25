f25e115b071e633f0214bfe787065f9521dd01b0
Nguyen Swan <149929360+NguyenSwan@users.noreply.github.com>
2025-02-25 09:46:05 +0700

    refactor: update image cccd

--- src/pages/ShipperDetails.jsx
+++ src/pages/ShipperDetails.jsx
@@ -1,4 +1,5 @@
-import { Button } from "@mantine/core";
+import { Button, Modal, Select, Textarea } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useShipper } from "../hooks/useShippers";
@@ -7,6 +8,12 @@ export default function ShipperDetails() {
     const { id } = useParams();
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
+    const [deactivationReason, setDeactivationReason] = useState("");
+    const [deactivationDuration, setDeactivationDuration] = useState("1 tháng");
+    const [customDate, setCustomDate] = useState(null);
+    const [deactivationDate, setDeactivationDate] = useState(null);
+    const [opened, setOpened] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
     const { data: shipper, error } = useShipper(id);
 
     if (!shipper) {
@@ -14,21 +21,47 @@ export default function ShipperDetails() {
     }
 
     const handleChangeStatus = (status) => {
+        if (status === "Dừng hoạt động") {
+            setOpened(true);
+        } else {
+            setIsLoading(true);
+            fetch(`http://localhost:3000/shippers/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ status }),
+            })
+                .then(() => {
+                    setIsLoading(false);
+                    navigate("/main/shipperslist");
+                })
+                .catch(() => setIsLoading(false));
+        }
+    };
+
+    const handleConfirmDeactivation = () => {
         setIsLoading(true);
+        const requestBody = {
+            status: "Dừng hoạt động",
+            deactivationReason,
+            deactivationDuration:
+                deactivationDuration === "Tùy chỉnh" ? customDate : deactivationDuration,
+        };
+
         fetch(`http://localhost:3000/shippers/${id}`, {
             method: "PATCH",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ status }),
+            body: JSON.stringify(requestBody),
         })
             .then(() => {
                 setIsLoading(false);
+                setOpened(false);
                 navigate("/main/shipperslist");
             })
             .catch(() => setIsLoading(false));
     };
 
     return (
-        <div className="flex items-center justify-center pt-10 ">
+        <div className="flex items-center justify-center pt-10">
             <div className="container px-4">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
                     <div className="p-6 text-center bg-white rounded-lg shadow-md">
@@ -41,7 +74,6 @@ export default function ShipperDetails() {
                         <h5 className="mt-4 text-lg font-semibold">{shipper.name}</h5>
                         <h6 className="text-sm text-gray-500">{shipper.email}</h6>
                         <div className="mt-6">
-                            {/* <h5 className="text-lg font-semibold">Trạng thái</h5> */}
                             <div
                                 className={`inline-block px-2 py-1 rounded-md text-sm font-semibold ${
                                     shipper.status === "Đang hoạt động"
@@ -75,9 +107,6 @@ export default function ShipperDetails() {
                                 <span className="font-bold">SĐT:</span> {shipper.phone}
                             </div>
                             <div>
-                                <span className="font-bold">CCCD:</span> {shipper.cccd}
-                            </div>
-                            <div>
                                 <span className="font-bold">Vị trí hoạt động:</span>{" "}
                                 {shipper.activityArea}
                             </div>
@@ -85,7 +114,72 @@ export default function ShipperDetails() {
                                 <span className="font-bold">Phương tiện:</span>{" "}
                                 {shipper.shippingMethod}
                             </div>
+                            <div>
+                                <span className="font-bold">CCCD:</span> {shipper.cccd}
+                            </div>
                         </div>
+
+                        <div className="mt-6">
+                            <h3 className="font-bold">Ảnh căn cước công dân (CCCD):</h3>
+                            <div className="flex gap-4 mt-2">
+                                <div className="relative">
+                                    {" "}
+                                    {/* Added relative wrapper for positioning */}
+                                    <img
+                                        src={shipper.idCardFrontFile}
+                                        alt="Mặt trước CCCD"
+                                        className="w-32 h-20 border cursor-pointer"
+                                        onClick={() => setSelectedImage(shipper.idCardFrontFile)}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter" || e.key === " ")
+                                                setSelectedImage(shipper.idCardFrontFile);
+                                        }}
+                                        aria-label="CCCD Mặt trước" // Add an accessible label
+                                    />
+                                    <span className="absolute px-1 text-xs text-white bg-black bg-opacity-50 rounded bottom-2 left-2">
+                                        Mặt trước
+                                    </span>{" "}
+                                    {/* Added label */}
+                                </div>
+                                <div className="relative">
+                                    {" "}
+                                    {/* Added relative wrapper for positioning */}
+                                    <img
+                                        src={shipper.idCardBackFile}
+                                        alt="Mặt sau CCCD"
+                                        className="w-32 h-20 border cursor-pointer"
+                                        onClick={() => setSelectedImage(shipper.idCardBackFile)}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter" || e.key === " ")
+                                                setSelectedImage(shipper.idCardBackFile);
+                                        }}
+                                        aria-label="CCCD Mặt sau" // Add an accessible label
+                                    />
+                                    <span className="absolute px-1 text-xs text-white bg-black bg-opacity-50 rounded bottom-2 left-2">
+                                        Mặt sau
+                                    </span>{" "}
+                                    {/* Added label */}
+                                </div>
+                            </div>
+                        </div>
+
+                        {selectedImage && (
+                            <div
+                                className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+                                onClick={() => setSelectedImage(null)}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter" || e.key === " ") setSelectedImage(null);
+                                }}
+                                aria-label="Xem ảnh CCCD" // Add an accessible label
+                            >
+                                <img
+                                    src={selectedImage}
+                                    alt="Ảnh CCCD"
+                                    className="max-w-full max-h-full p-4 bg-white rounded-lg shadow-lg"
+                                />
+                            </div>
+                        )}
+
                         <h6 className="mt-6 mb-4 font-semibold text-blue-500">Liên lạc khẩn cấp</h6>
                         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                             <div>
@@ -104,16 +198,18 @@ export default function ShipperDetails() {
                         <div className="flex items-center justify-center mt-6 space-x-4">
                             <div className="text-left">
                                 <Button
-                                    color={shipper?.status === "Active" ? "red" : "teal"}
+                                    color={shipper?.status === "Đang hoạt động" ? "red" : "teal"}
                                     onClick={() =>
                                         handleChangeStatus(
-                                            shipper?.status === "Active" ? "Inactive" : "Active",
+                                            shipper?.status === "Đang hoạt động"
+                                                ? "Dừng hoạt động"
+                                                : "Đang hoạt động",
                                         )
                                     }
                                 >
                                     {isLoading ? (
                                         <span>Đang xử lý...</span>
-                                    ) : shipper?.status === "Active" ? (
+                                    ) : shipper?.status === "Đang hoạt động" ? (
                                         "Dừng hoạt động"
                                     ) : (
                                         "Kích hoạt"
@@ -129,6 +225,43 @@ export default function ShipperDetails() {
                     </div>
                 </div>
             </div>
+            <Modal
+                opened={opened}
+                onClose={() => setOpened(false)}
+                title="Bạn xác nhận cho người này dừng hoạt động không?"
+            >
+                <Textarea
+                    label="Lý do dừng hoạt động"
+                    value={deactivationReason}
+                    onChange={(e) => setDeactivationReason(e.target.value)}
+                    placeholder="Nhập lý do..."
+                    rows={4}
+                />
+                <div className="flex justify-center text-center">
+                    <Select
+                        label="Thời gian tạm dừng"
+                        value={deactivationDuration}
+                        onChange={setDeactivationDuration}
+                        data={["1 tháng", "3 tháng", "6 tháng", "Vĩnh viễn", "Tùy chỉnh"]}
+                    />
+                    {deactivationDuration === "Tùy chỉnh" && (
+                        <DatePicker
+                            label="Chọn ngày kết thúc"
+                            value={customDate}
+                            onChange={setCustomDate}
+                            placeholder="Chọn ngày"
+                        />
+                    )}
+                </div>
+                <div className="flex justify-end mt-4 space-x-4">
+                    <Button color="red" onClick={handleConfirmDeactivation} loading={isLoading}>
+                        Xác nhận
+                    </Button>
+                    <Button variant="outline" onClick={() => setOpened(false)}>
+                        Hủy
+                    </Button>
+                </div>
+            </Modal>
         </div>
     );
 }

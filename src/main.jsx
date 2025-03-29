0febdee9dee1b5827fb25ec8a6ec705e720fa090
Nguyen Swan <149929360+NguyenSwan@users.noreply.github.com>
2025-03-30 03:28:44 +0700

    feat: update

--- src/main.jsx
+++ src/main.jsx
@@ -48,6 +48,8 @@ import ShopsRevenuePage from "./pages/ShopsRevenuePage.jsx";
 import UsersPage from "./pages/UsersPage.jsx";
 import UserDetailPage from "./pages/ui-customer-detail/UserDetailPage.jsx";
 import { Button } from "@mantine/core";
+import BannerList from "./pages/BannerList.jsx";
+import BannerDetail from "./pages/BannerDetail.jsx";
 const theme = createTheme({
     colors: {
         // cGreen = custom green
@@ -208,6 +210,14 @@ const router = createBrowserRouter([
                 path: "*",
                 element: <ErrorPage />,
             },
+            {
+                path: "/main/banners",
+                element: <BannerList />,
+            },
+            {
+                path: "/main/banners/:id",
+                element: <BannerDetail />,
+            }
         ],
     },
 ]);
